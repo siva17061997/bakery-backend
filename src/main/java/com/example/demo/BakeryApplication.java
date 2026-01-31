6417743320ae03f0b6f0029a44c29a27c65eae68
siva17061997 <sivawb678@gmail.com>
2026-01-31 16:19:09 +0530

    Fix Hibernate entity scan

--- src/main/java/com/example/demo/BakeryApplication.java
+++ src/main/java/com/example/demo/BakeryApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.demo")
+@EnableJpaRepositories(basePackages = "com.example.demo")
 public class BakeryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BakeryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryApplication.class, args);
+    }
 }
